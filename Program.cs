21af2fa0576e6fd682045ef83b2b19808c8df358
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-12-07 09:49:37 +0100

    Fixed bug in route mapping

--- Program.cs
+++ Program.cs
@@ -36,6 +36,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
